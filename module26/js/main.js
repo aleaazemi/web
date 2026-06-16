a0d2cdab1df9886c1f9830fa3bd9bd3295550b8c
aleaazemi <aleaazemi9@gmail.com>
2026-06-16 17:47:47 +0200

    commit

---
+++ module26/js/main.js
@@ -0,0 +1,61 @@
+console.log("hello from oltiona")
+
+var x=5;
+
+var y=28+43;
+console.log(y);
+
+var b=64%21;
+console.log(b);
+
+var c=99;
+c++;
+console.log(c);
+
+var c=99;
+c--;
+console.log(c);
+
+var q=4;
+var p=q**2;
+console.log(p);
+
+
+var b=6;
+b*=2;
+console.log(b);
+
+var b=6;
+var a=7;
+console.log(a==b);
+
+
+
+console.log("-------------");
+
+var b=7;
+var a="7";
+console.log(a===b);
+
+var b=6;
+var a=7;
+console.log(a>b);
+
+var b=6;
+var a=74;
+console.log(a<b);
+
+var b=6;
+var a=6;
+console.log(a!=b);
+
+var name="oltiona";
+var lastname="qadraku";
+var fullname= name+ " " + lastname;
+console.log(fullname);
+
+var age=24;
+console.log(age>=18 && age>=65);//dy kushte sakta
+
+var age=23;
+console.log(age<=22 || age<=65);// mjafton 1 e sakkt
---
+++ module27/js/main.js
@@ -0,0 +1,9 @@
+function kthesms(){
+    alert("ky funksion kthen nje sms")
+}
+kthesms()
+
+function sum(n1,n2,n3){//parametra qe i tegojn funksionit qfar vlera pret
+    return n1+n2+n3
+}
+console.log(sum(2,5,7))
